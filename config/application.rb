5bd548d055fc48427cf62ad79bc705a4454fa765
ailobe <aitorlopezbeltran@gmail.com>
2019-06-05 10:14:00 +0200

    Apply requested changes

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module DiputacioGironaApp
 
     config.active_job.queue_adapter = :delayed_job
 
+    # Cannot force time_zone as all timestamps in DB are in UTC
+    # and ought to be updated if this config was applied.
+    # config.time_zone = 'Madrid'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
